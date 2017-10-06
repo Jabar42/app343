2ef917c717c131c954a328828d7a32b8847766ea
jabar42 <jaimebarbudomier@gmail.com>
2017-10-06 01:41:43 -0400

    agregados los componentes del content y footer, compilando bien

---
+++ src/components/global/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <h1> Soy el Footer </h1>
+	<p> Loremp ipsum...</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
